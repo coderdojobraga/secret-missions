2a97d036d7456df58546c62ded78079c4a42b043
0urobor0s <lhome.gamer@gmail.com>
2018-12-09 15:40:51 +0000

    [Secret Mission] Dice roller

---
+++ diceroller/solution.rb
@@ -0,0 +1,29 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
+
+class DiceRoller
+  def roll_b2(input)
+    results = []
+    num, sides = input.split("d").map(&:to_i)
+    num.times do
+      results.push(rand(2..sides))
+    end
+    puts "#{results.sum}: #{results.map(&:to_s).join(" ")}"
+  end
+
+  def roll(input)
+    result = 0
+    num, sides = input.split("d").map(&:to_i)
+    num.times do
+      result += rand(2..sides)
+    end
+    puts result
+  end
+
+  def read(file)
+    File.foreach(file) do |x| 
+      roll_b2(x) 
+    end
+  end
+end
+
+DiceRoller.new.read(ARGV[0])
